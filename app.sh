2fcf46f6a482111b02dd38390942797aaca0d2b8
DouShaoxun <dousx_cn@163.com>
2022-05-09 19:35:13 +0800

    sh

--- app.sh
+++ app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 示例 其中 -a和-h可默认不传 -c不传默认是status
+# ./boot-app.sh -c start
+# ./boot-app.sh -c start -a prod -h 512
 # 命令
 COMMAND=status
 
@@ -10,10 +13,30 @@ PROJECT_DIR=/home/dousx/cpu-take-up-high-test
 JAR_PATH=${PROJECT_DIR}/cpu-take-up-high-test-snapshot.jar
 
 # spring boot配置 dev/prod等
-# 传入或者写死
-ACTIVE=prod
+ACTIVE=dev
+
+#  -Xms256m -Xmx256m 单位M
+HEAP_MEMORY=256
 
 
+#  ./app.sh  -c stat -h 122
+while getopts ":c:a:h:" opt
+do
+ case $opt in
+  c)
+  COMMAND=$OPTARG
+  ;;
+  a)
+  ACTIVE=$OPTARG
+  ;;
+  h)
+  HEAP_MEMORY=$OPTARG
+  ;;
+  ?)
+  echo -e " \033[31m Unknown Parameter.\033[0m"
+  exit 1;;
+ esac
+done
 
 
 usage() {
@@ -47,14 +70,15 @@ start() {
   is_run
   if [ $? -eq "0" ]; then
     echo -e "${JAR_PATH} \033[32m is already running.\033[0m \033[33m pid = ${pid}\033[0m "
+    echo -e "If you need to stop,please execute\033[33m  ./app.sh -c stop \033[0m"
   else
     echo "ready to start"
-    echo "nohup java -Xms512m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError \ "
+    echo "nohup java -Xms${HEAP_MEMORY}m -Xmx${HEAP_MEMORY}m -XX:+HeapDumpOnOutOfMemoryError \ "
     echo "-XX:HeapDumpPath=./ -jar  \  "
     echo "-Dspring.profiles.active=${ACTIVE} \ "
     echo "${JAR_PATH} >/dev/null 2>&1 & "
 
-    nohup java -Xms512m -Xmx1024m  -XX:+HeapDumpOnOutOfMemoryError \
+    nohup java -Xms${HEAP_MEMORY}m -Xmx${HEAP_MEMORY}m  -XX:+HeapDumpOnOutOfMemoryError \
       -XX:HeapDumpPath=./  -jar \
       -Dspring.profiles.active=${ACTIVE} \
       ${JAR_PATH} >/dev/null 2>&1 &
@@ -84,8 +108,10 @@ status() {
   if [ $? -eq "0" ]; then
     echo -e "${JAR_PATH} \033[32m is already running.\033[0m \033[33m pid = ${pid}\033[0m "
     parameter
+    echo -e "If you need to stop,please execute\033[33m  ./app.sh -c stop \033[0m"
   else
-    echo -e "${JAR_PATH} \033[31m is not running.\033[0m"
+    echo -e "${JAR_PATH}\033[31m is not running.\033[0m"
+    echo -e "If you need to start,please execute\033[33m  ./app.sh -c start \033[0m"
   fi
 }
 
